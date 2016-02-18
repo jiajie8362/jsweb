b4161520f38ee2d4ec5a20afce632d8c2acbaa90
jia jie <jiajie8362@gmail.com>
2016-02-18 19:19:20 +0800

    [JJ] git config --global alias.co checkout

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -13,14 +13,17 @@ spa.shell = (function () {
      + '<div class="spa-shell-chat"></div>'
      + '<div class=="spa-shell-modal"></div>',
 
-     chat_extend_time: 1000,
+     chat_extend_time: 250,
      chat_retract_time: 300,
      chat_entend_height: 450,
      chat_retract_height: 15,
      chat_extended_title: 'Click to retract',
      chat_retracted_title: 'Click to extend'
   },
-  stateMap = { $container: null },
+  stateMap = {
+    $container: null,
+    is_chat_retracted: true
+  },
   jqueryMap = {},
   setJqueryMap, toggleChat, initModule;
 
@@ -50,7 +53,8 @@ spa.shell = (function () {
         function() {
           jqueryMap.$chat.attr(
             'title', configMap.chat_extended_title
-          )
+          );
+          stateMap.is_chat_retracted = false;
           if (callback) {
             callback(jqueryMap.$chat);
           }
@@ -63,18 +67,31 @@ spa.shell = (function () {
       { height: configMap.chat_retract_height },
       configMap.chat_retract_time,
       function() {
+        jqueryMap.$chat.attr(
+          'title', configMap.chat_retracted_title
+        );
+        stateMap.is_chat_retracted = true;
         if (callback) {
           callback(jqueryMap.$chat);
         }
       }
     );
     return true;
-  }
+  };
+
+  onClickChat = function(event) {
+    toggleChat(stateMap.is_chat_retracted);
+    return false;
+  };
 
   initModule = function ($container) {
     stateMap.$container = $container;
     $container.html( configMap.main_html );
     setJqueryMap();
+    stateMap.is_chat_retracted = true;
+    jqueryMap.$chat
+      .attr('title', configMap.chat_retracted_title)
+      .click(onClickChat);
     setTimeout( function() {toggleChat(true);}, 3000);
     setTimeout( function() {toggleChat(false);}, 8000);
   };
