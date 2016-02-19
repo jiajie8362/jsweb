f9b6cfa8b70fb5716e74b00cbccb01a68f06c2ea
jia jie <jiajie8362@gmail.com>
2016-02-19 16:40:33 +0800

    [JJ] unfinish chapter 3

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -1,5 +1,8 @@
 spa.shell = (function () {
   var configMap = {
+    anchor_schema_map: {
+      chat: {open:true, closed: true}
+    },
     main_html: '<div class="spa-shell-head">'
      + '<div class="spa-shell-head-logo"></div>'
      + '<div class="spa-shell-head-acct"></div>'
@@ -22,10 +25,80 @@ spa.shell = (function () {
   },
   stateMap = {
     $container: null,
+    anchor_map: {},
     is_chat_retracted: true
   },
   jqueryMap = {},
-  setJqueryMap, toggleChat, initModule;
+  copyAnchorMap, setJqueryMap, toggleChat,
+  changeAnchorPart, onHashchange,
+  onClickChat, initModule;
+
+  copyAnchorMap = function () {
+    return $.extend( true, {}, stateMap.anchor_map );
+  };
+
+  changeAnchorPart = function(arg_map) {
+    var
+      anchor_map_revise = copyAnchorMap(),
+      bool_return = true,
+      key_name, key_name_dep;
+      KEYVAL:
+      for(key_name in arg_map) {
+        if(arg_map.hasOwnProperty(key_name)) {
+          if(key_name.indexOf('_') === 0) {continue KEYVAL;}
+          anchor_map_revise[key_name] = arg_map[key_name];
+          key_name_dep = '_' + key_name;
+          if(arg_map[key_name_dep]) {
+            anchor_map_revise[key_name_dep] = arg_map[key_name_dep];
+          }
+          else {
+            delete anchor_map_revise[key_name_dep];
+            delete anchor_map_revise['_s' + key_name_dep];
+          }
+        }
+      }
+      try {
+        $.uriAnchor.setAnchor(anchor_map_revise);
+      }
+      catch(error) {
+        $.uriAnchor.setAnchor(stateMap.anchor_map, null, true);
+        bool_return = false;
+      }
+      return bool_return;
+  };
+
+  onHashchange = function(event) {
+    var
+      anchor_map_previous = copyAnchorMap(),
+      anchor_map_proposed,
+      _s_chat_previous, _s_chat_proposed,
+      s_chat_proposed;
+      try { anchor_map_proposed = $.uriAnchor.makeAnchorMap(); }
+      catch (error) {
+        $.uriAnchor.setAnchor( anchor_map_previous, null, true);
+        return false;
+      }
+      stateMap.anchor_map = anchor_map_proposed;
+      _s_chat_previous = anchor_map_previous._s_chat;
+      _s_chat_proposed = anchor_map_proposed._s_chat;
+
+      if( !anchor_map_previous || _s_chat_previous !== _s_chat_proposed) {
+        s_chat_proposed = anchor_map_proposed.chat;
+        switch(s_chat_proposed) {
+          case 'open':
+            toggleChat(true);
+            break;
+          case 'closed':
+            toggleChat(false);
+            break;
+          default:
+            toggleChat(false);
+            delete anchor_map_proposed.chat;
+            $.uriAnchor.setAnchor(anchor_map_proposed, null ,true);
+        }
+      }
+      return false;
+  };
 
   setJqueryMap = function () {
     var $container = stateMap.$container;
@@ -36,7 +109,7 @@ spa.shell = (function () {
   };
 
   toggleChat = function(do_extend, callback) {
-    var px_chat_ht = jqueryMap.$chat.height(),
+    var px_chat_ht = jqueryMap.$chat.height（）,
       is_open = px_chat_ht === configMap.chat_entend_height,
       is_closed = px_chat_ht === configMap.chat_retract_height,
       is_sliding = !is_open && !is_closed;
@@ -80,22 +153,18 @@ spa.shell = (function () {
   };
 
   onClickChat = function(event) {
-    toggleChat(stateMap.is_chat_retracted);
+    changeAnchorPart({
+      chat: (stateMap.is_chat_retracted ? 'open' : 'closed')
+    });
     return false;
   };
 
   initModule = function ($container) {
-    stateMap.$container = $container;
-    $container.html( configMap.main_html );
-    setJqueryMap();
-    stateMap.is_chat_retracted = true;
-    jqueryMap.$chat
-      .attr('title', configMap.chat_retracted_title)
-      .click(onClickChat);
-    setTimeout( function() {toggleChat(true);}, 3000);
-    setTimeout( function() {toggleChat(false);}, 8000);
+    $.uriAnchor.configModule({
+      schema_map: configMap.anchor_schema_map
+    });
+    $(window).bind('hashchange', onHashchange).trigger('hashchange');
   };
-
 
   return { initModule: initModule };
 }());
